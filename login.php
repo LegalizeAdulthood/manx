7632a8cd04776e4223b6a4fff31b590c66e2fcc5
Richard <legalize@xmission.com>
2010-05-14 20:46:27 -0600

    manx : Implemented : Fix internal references between files

--- login.php
+++ login.php
@@ -4,14 +4,14 @@
 <body>
 <body id="VT100-NET">
 <div id="AUTH">Guest | <a href="login.php">Login</a></div><div id="LOGO"><h1><span>Manx &ndash; a catalogue of online computer manuals</span></h1></div><div id="MENU"><A CLASS="first" href="default.php">Search</a><span class="nodisp"> | </span><a href="about.php">About</A><SPAN CLASS="nodisp"> | </SPAN><A HREF="help.php">Help</A></DIV>
-<div class="det"><form id="LOGINFORM" method="post" action="http://vt100.netlogin.php">
+<div class="det"><form id="LOGINFORM" method="post" action="login.php">
 <table id="LOGINBOX">
 <tbody>
 <tr><td><label for="USERFIELD">Username:</label></td>
 <td><input type="text" id="USERFIELD" name="user" size="20" value=""></td></tr>
 <tr><td><label for="PASSFIELD">Password:</label></td>
 <td><input type="password" id="PASSFIELD" name="pass" size="20"></td></tr>
-<tr><td colspan="2"><input type="submit" id="LOGIBUTT" name="LOGI" value="Login"><input type="hidden" name="redirect" value="http://vt100.nethelp.php?q=;cp=1"></td></tr>
+<tr><td colspan="2"><input type="submit" id="LOGIBUTT" name="LOGI" value="Login"><input type="hidden" name="redirect" value="help.php?q=;cp=1"></td></tr>
 </tbody></table></form>
 
 </div></body></html>
