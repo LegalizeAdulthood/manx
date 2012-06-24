32b27135d7b57494d7f2067688caa10a6e5feb08
Richard <legalize@xmission.com>
2012-06-24 14:43:25 -0600

    manx : Fixed : Correct errors introduced by refactoring

--- mirror.php
+++ mirror.php
@@ -15,7 +15,7 @@ class MirrorPage extends AdminPageBase
 		print "<h1>Mirrors</h1>\n";
 		print "<table border=\"1\">\n";
 		print "<tr><th>Id</th><th>Site</th><th>Original Stem</th><th>Copy Stem</th><th>Rank</th></tr>\n";
-		foreach ($this->_manx->getMirrors() as $mirror)
+		foreach ($this->_manxDb->getMirrors() as $mirror)
 		{
 			printf("<tr><td align=\"center\">%d</td><td align=\"center\">%d</td><td>%s</td><td>%s</td><td align=\"center\">%d</td></tr>\n",
 				$mirror['mirror_id'], $mirror['site'], $mirror['original_stem'], $mirror['copy_stem'], $mirror['rank']);
