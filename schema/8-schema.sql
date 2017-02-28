8d7a2343b070848d1f69ef5dbfc3c709f1022f6e
Richard <legalize@xmission.com>
2017-02-27 21:46:15 -0700

    manx : Implemented : Add parent directories to site_unknown_dir

--- schema/8-schema.sql
+++ schema/8-schema.sql
@@ -6,9 +6,10 @@ DROP TABLE IF EXISTS `site_unknown_dir`;
 CREATE TABLE IF NOT EXISTS `site_unknown_dir` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `site_id` INT(11) NOT NULL,
+  `parent_id` INT(11) NOT NULL,
   `directory` VARCHAR(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
-  UNIQUE KEY (`site_id`, `directory`)
+  UNIQUE KEY (`site_id`, `parent_id`, `directory`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 ---
@@ -27,6 +28,24 @@ INSERT INTO `site_unknown_dir`(`site_id`, `directory`)
     `site_id`,
     LEFT(`path`, LENGTH(`path`)-LENGTH(SUBSTRING_INDEX(REVERSE(`path`), '/', 1))-1) as `directory`
     FROM `site_unknown`;
+-- insert root dirs (site_id,parent_id=-1,directory='')
+-- selecct all rows with parent_id=0 and matching dirs to tmp
+-- while (rows in tmp) do
+--   update site_unknown_dir with rows from tmp
+--   drop all rows in tmp
+--   select all rows with parent_id=0 and matching dirs to tmp
+-- end while
+-- select all rows with parent_id=0 and parent_dir to tmp
+-- while (rows in tmp) do
+--   insert all parent dirs
+--   update parent_dir_id of all rows in tmp
+--   select all rows with parent_id=0 and parent_dir to tmp
+-- end while
+UPDATE `site_unknown_dir` SET
+  `parent_id` = (
+    SELECT `id` AS `parent_id` FROM `site_unknown_dir`
+      WHERE `directory`=LEFT(`directory`, LENGTH(`directory`)-LENGTH(SUBSTRING_INDEX(REVERSE(`path`), '/', 1))-1)
+  );
 
 --
 -- Populate site_unknown_dir_id and file_name
