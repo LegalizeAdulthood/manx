83c93e919e518c13e32a4658ad680244293888fe
Richard <legalize@xmission.com>
2019-09-29 01:54:42 -0600

    Reverse test failure

--- test/TestUrlWizardPage.php
+++ test/TestUrlWizardPage.php
@@ -102,7 +102,6 @@ class TestUrlWizardPage extends PHPUnit\Framework\TestCase
         $page->postPage();
         $output = ob_get_contents();
 
-        $this->assertFalse(true);
         $this->assertFalse($db->addCompanyCalled);
         $this->assertTrue($this->_manx->addPublicationCalled);
         $this->assertEquals($vars['pub_history_ph_title'], $this->_manx->addPublicationLastTitle);
