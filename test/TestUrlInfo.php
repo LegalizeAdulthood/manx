9edfbc1a5cdc5c0bf6f4f5a2d337cddc28738fd4
Richard <legalize@xmission.com>
2019-09-29 19:33:18 -0600

    Switch tests to use phpunit mocks

--- test/TestUrlInfo.php
+++ test/TestUrlInfo.php
@@ -1,66 +1,63 @@
 <?php
 
-require_once 'test/FakeCurlApi.php';
 require_once 'pages/UrlInfo.php';
 
 class TestUrlInfo extends PHPUnit\Framework\TestCase
 {
     public function testConstruct()
     {
-        $curlApi = new FakeCurlApi();
+        $curlApi = $this->createMock(ICurlApi::class);
+        $curlApi->expects($this->never())->method('init');
+
         $curl = new UrlInfo(null, $curlApi);
-        $this->assertFalse($curlApi->initCalled);
+
         $this->assertNotNull($curl);
     }
 
-    public function testSizeReturns4096()
+    public function testSizeReturnsContentLength()
     {
-        $curlApi = new FakeCurlApi();
-        $curlApi->initFakeResult = 0xdeadbeef;
-        $curlApi->execFakeResult = "HTTP/1.0 200 OK\n"
+        $curlApi = $this->createMock(ICurlApi::class);
+        $curlApi->expects($this->once())->method('init')->willReturn(0xdeadbeef);
+        $curlApi->expects($this->once())->method('exec')->willReturn("HTTP/1.0 200 OK\n"
             . "Content-Length: 4096\n"
-            . "\n";
-        $curlApi->getinfoFakeResult = 200;
+            . "\n");
+        $curlApi->expects($this->once())->method('getinfo')->willReturn(200);
         $url = 'http://bitsavers.org/WhatsNew.txt';
         $curl = new UrlInfo($url, $curlApi);
+
         $size = $curl->size();
-        $this->assertTrue($curlApi->initCalled);
-        $this->assertTrue($curlApi->setoptCalled);
-        $this->assertTrue($curlApi->execCalled);
-        $this->assertTrue($curlApi->getinfoCalled);
-        $this->assertTrue($curlApi->closeCalled);
+
         $this->assertEquals(4096, $size);
     }
 
     public function testGetLastModified()
     {
-        $curlApi = new FakeCurlApi();
-        $curlApi->initFakeResult = 0xdeadbeef;
-        $curlApi->execFakeResult = "HTTP/1.0 200 OK\n"
+        $curlApi = $this->createMock(ICurlApi::class);
+        $curlApi->method('init')->willReturn(0xdeadbeef);
+        $curlApi->method('exec')->willReturn("HTTP/1.0 200 OK\n"
             . "Last-Modified: Wed, 15 Nov 1995 04:58:08 GMT\n"
-            . "\n";
-        $curlApi->getinfoFakeResult = 200;
+            . "\n");
+        $curlApi->method('getinfo')->willReturn(200);
         $url = 'http://bitsavers.org/Whatsnew.txt';
         $curl = new UrlInfo($url, $curlApi);
+
         $lastModified = $curl->lastModified();
-        $this->assertTrue($curlApi->initCalled);
-        $this->assertTrue($curlApi->setoptCalled);
-        $this->assertTrue($curlApi->execCalled);
-        $this->assertTrue($curlApi->getinfoCalled);
-        $this->assertTrue($curlApi->closeCalled);
+
         $this->assertEquals(strtotime('Wed, 15 Nov 1995 04:58:08 GMT'), $lastModified);
     }
 
     public function test404ErrorGivesSizeOfFalse()
     {
-        $curlApi = new FakeCurlApi();
-        $curlApi->initFakeResult = 0xdeadbeef;
-        $curlApi->execFakeResult = "HTTP/1.0 404 Not found\n"
-            . "\n";
-        $curlApi->getinfoFakeResult = 404;
+        $curlApi = $this->createMock(ICurlApi::class);
+        $curlApi->method('init')->willReturn(0xdeadbeef);
+        $curlApi->method('exec')->willReturn("HTTP/1.0 404 Not found\n"
+            . "\n");
+        $curlApi->method('getinfo')->willReturn(404);
         $url = 'http://foo.example.com/bogusDocument.pdf';
         $curl = new UrlInfo($url, $curlApi);
+
         $size = $curl->size();
+
         $this->assertTrue($size === false);
     }
 }
