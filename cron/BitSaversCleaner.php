70b807d778ccd0c4e8bd29215dbc82ab9f90ad4a
Richard <legalize@xmission.com>
2019-11-16 18:52:17 +0000

    Use BitSaversConfig for cron processor

--- cron/BitSaversCleaner.php
+++ cron/BitSaversCleaner.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'cron/WhatsNewCleaner.php';
+require_once 'pages/BitSaversConfig.php';
 require_once 'pages/WhatsNewIndex.php';
 
 use Pimple\Container;
@@ -9,12 +10,7 @@ class BitSaversCleaner extends WhatsNewCleaner
 {
     public function __construct(Container $config)
     {
-        $config['siteName'] = 'bitsavers';
-        $config['timeStampProperty'] = 'bitsavers_whats_new_timestamp';
-        $config['indexByDateFile'] = 'bitsavers-IndexByDate.txt';
-        $config['indexByDateUrl'] = 'http://bitsavers.trailing-edge.com/pdf/IndexByDate.txt';
-        $config['baseCheckUrl'] = 'http://bitsavers.trailing-edge.com/pdf';
-        $config['baseUrl'] = 'http://bitsavers.org/pdf';
+        BitSaversConfig::configure($config);
         parent::__construct($config);
     }
 }
