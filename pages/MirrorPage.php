abe4d4330d00dd32094590eddd12d1a074e703db
Richard <legalize@xmission.com>
2019-12-25 00:59:15 +0000

    Extract File MirrorPage.php from mirror.php

---
+++ pages/MirrorPage.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Manx;
+
+require_once 'vendor/autoload.php';
+
+use Pimple\Container;
+
+class MirrorPage extends AdminPageBase
+{
+    protected function getMenuType()
+    {
+        return MenuType::Mirror;
+    }
+
+    protected function renderBodyContent()
+    {
+        print "<h1>Mirrors</h1>\n";
+        print "<table border=\"1\">\n";
+        print "<tr><th>Id</th><th>Site</th><th>Original Stem</th><th>Copy Stem</th><th>Rank</th></tr>\n";
+        foreach ($this->_manxDb->getMirrors() as $mirror)
+        {
+            printf("<tr><td align=\"center\">%d</td><td align=\"center\">%d</td><td>%s</td><td>%s</td><td align=\"center\">%d</td></tr>\n",
+                $mirror['mirror_id'], $mirror['site'], $mirror['original_stem'], $mirror['copy_stem'], $mirror['rank']);
+        }
+        print "</table>\n";
+    }
+
+    protected function postPage()
+    {
+        throw new Exception("postPage unimplemented");
+    }
+}
