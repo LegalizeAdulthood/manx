7b617c75179c1edcd685d85b80adfb723671e9fe
Richard <legalize@xmission.com>
2021-10-01 23:59:43 +0000

    vendor directory is peer with public

--- public/api.php
+++ public/api.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
